44a925933d85ee6e2102e70d3fbb05984590843d
ccastano46 <castanocamilo522@gmail.com>
2025-04-04 09:36:29 -0500

    Bid polling

--- src/main/java/arsw/tamaltolimense/bidservice/BidServiceApplication.java
+++ src/main/java/arsw/tamaltolimense/bidservice/BidServiceApplication.java
@@ -2,8 +2,13 @@ package arsw.tamaltolimense.bidservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableScheduling
 public class BidServiceApplication {
 
 	public static void main(String[] args) {
